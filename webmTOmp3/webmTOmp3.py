a96650a26f9baa53ba17f50f25a30f9d7c2e8a55
k.godales <k.godales@chelpipegroup.com>
2025-07-18 18:27:28 +0300

    to mp3
    to mp3

---
+++ webmTOmp3/webmTOmp3.py
@@ -0,0 +1,13 @@
+﻿from moviepy.editor import VideoFileClip
+# Ruta al archivo .webm
+input_path = "Запись встречи 16.07.2025 15-15-57 - запись.webm"
+output_path = "audio.mp3"
+try:
+    # Cargar el video
+    video = VideoFileClip(input_path)
+except Exception as e:
+    print(e) 
+# Extraer y guardar el audio en formato mp3
+video.audio.write_audiofile(output_path)
+print("Conversión completada:", output_path)
+
